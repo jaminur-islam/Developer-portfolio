e2607f93bced98e27025cccc8077dc85cef9c17a
Jaminur islam <jaminurislam250@gmail.com>
2021-07-05 18:52:55 +0600

    javaadd

--- index.js
+++ index.js
@@ -2,58 +2,57 @@ let headerSection = document.querySelector('.header-section');
 let rightArea = document.querySelector('.right-area');
 let button = document.getElementById('button');
 
-function generateColor () {              
-    let haxColors = ['0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , 'a' , 'b' , 'c' , 'd' , 'e' , 'f'];
+function generateColor() {
+    let haxColors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
     let color = '#';
-  for ( let i = 0 ;  i < 6  ;  i++) {       
-    let num = Math.round(Math.random()*15);
-    let num2 = haxColors[num];
-     color += num2;
-     console.log(color);
-  }
-  
-  rightArea.style.backgroundColor = color;
-
-  
-    }
-   
-   
-    function audioPlay(){ 
-   
-      
-      
-      
-      
-      
-            
-  var audio = new Audio('/valobasbore.mp3');
-  audio.play();
-
-      
-      
+    for (let i = 0; i < 6; i++) {
+        let num = Math.round(Math.random() * 15);
+        let num2 = haxColors[num];
+        color += num2;
+        console.log(color);
     }
 
-    headerSection.addEventListener("click", generateColor )
+    rightArea.style.backgroundColor = color;
 
 
-    
-    var num = 0;
-
-    headerSection.addEventListener("click", function (){
-        
-        num++;
-        console.log(typeof num)
-
-       
-        if(num === 1) {
-            var audio = new Audio("/valobasbore.mp3");
-            audio.play();
-        }else if(num ===4){
-            alert("Oi beta koto bar click koros")
-        }
-    
-    
+}
 
 
-    } )
+function audioPlay() {
 
+
+
+
+
+
+
+    var audio = new Audio('/valobasbore.mp3');
+    audio.play();
+
+
+
+}
+
+headerSection.addEventListener("click", generateColor)
+
+
+
+var num = 0;
+
+headerSection.addEventListener("click", function () {
+
+    num++;
+    console.log(typeof num)
+
+
+    if (num === 1) {
+        var audio = new Audio("/valobasbore.mp3");
+        audio.play();
+    } else if (num === 4) {
+        alert("Oi beta koto bar click koros")
+    }
+
+
+
+
+})
